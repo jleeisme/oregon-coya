bdff69e65d05e11f1e52c23906971dd9e2c9df03
Jason Lee <jleeisme@gmail.com>
2016-09-16 16:44:21 -0700

    Added the ui file

--- js/UI.js
+++ js/UI.js
@@ -4,10 +4,168 @@ Oregon.UI = {};
  
 //show a notification in the message area
 Oregon.UI.notify = function(message, type){
-  console.log(message + ' - ' + type);
+  document.getElementById('updates-area').innerHTML = '<div class="update-' + type + '">Day '+ Math.ceil(this.caravan.day) +': ' + message+'</div>' + document.getElementById('updates-area').innerHTML;
 };
  
 //refresh visual caravan stats
 Oregon.UI.refreshStats = function() {
-  console.log(this.caravan);
-}
+  // modify DOM
+  document.getElementById('stat-day').innerHTML = Math.ceil(this.caravan.day);
+  document.getElementById('stat-distance').innerHTML = Math.floor(this.caravan.distance);
+  document.getElementById('stat-crew').innerHTML = this.caravan.crew;
+  document.getElementById('stat-oxen').innerHTML = this.caravan.oxen;
+  document.getElementById('stat-food').innerHTML = Math.ceil(this.caravan.food);
+  document.getElementById('stat-money').innerHTML = this.caravan.money;
+  document.getElementById('stat-firepower').innerHTML = this.caravan.firepower;
+  document.getElementById('stat-weight').innerHTML = Math.ceil(this.caravan.weight) + '/' + this.caravan.capacity;
+
+  // update caravan position
+  document.getElementById('caravan').style.left = (380 * this.caravan.distance/Oregon.FINAL_DISTANCE) + 'px';
+};
+
+// show shop
+Oregon.UI.showShop = function(prodcuts){
+
+  // get shop area
+  var shopDiv = document.getElementById('shop');
+  shopDiv.classList.remove('hidden');
+
+  // init the shop just once
+  if(!this.shopInitiated) {
+
+    // event delegation
+    shopDiv.addEventListener('click', function(e){
+      // what was clicked
+      var target = e.target || e.src;
+
+      // exit button
+      if(target.tagName == 'BUTTON') {
+        // resume journey
+        shopDiv.classList.add('hidden');
+        Oregon.UI.game.resumeJourney();
+      }
+      else if(target.tagName =='DIV' && target.className.match(/product/)) {
+        console.log('buying')
+
+        var bought = Oregon.UI.buyProduct({
+          item: target.getAttribute('data-item'),
+          qty: target.getAttribute('data-qty'),
+          price: taget.getAttribute('data-price')
+        });
+
+        if(bought) target.html = '';
+      } 
+    })
+    this.shopInitiated = true;
+  }
+
+  // clear existing content
+  var prodsDiv = document.getElementById('prods');
+  prodsDiv.innerHTML = '';
+
+  // show products
+  var product;
+  for(var i = 0; i < products.length; i++) {
+    product = products[i];
+    prodsDiv.innerHTML += '<div class="product" data-qty="' + product.qty + '" data-item="' + product.item + '" data-price"' + product.price + '">' + product.qty + ' ' + product.item + ' - $' + product.price + '</div>';
+  }
+
+  // setup click event
+  //document.getElementsByClassName('product').addEventListener(OregonH.UI.buyProduct);
+};
+
+// buy product
+Oregon.UI.buyProduct = function(product) {
+  // check if can afford
+  if(product.price > Oregon.UI.caravan.money) {
+    Oregon.UI.notify('Not enough money', 'negative');
+    return false;
+  }
+
+  Oregon.UI.caravan.money -= product.price;
+
+  Oregon.UI.caravan[product.item] += +product.qty;
+
+  Oregon.UI.notify('Bought ' + product.qty + ' x ' + product.item, 'positive');
+
+  // update weight
+  Oregon.UI.caravan.updateWeight();
+
+  // update visuals
+  Oregon.UI.refreshStats();
+
+  return true;
+};
+
+// show attack
+Oregon.UI.showAttack = function(firepower, gold) {
+  var attackDiv = document.getElementById('attack');
+  attackDiv.classList.remove('hidden');
+
+  // keep properties
+  this.firepower = firepower;
+  this.gold = gold;
+
+  // show firepower
+  document.getElementById('attack-description').innerHTML = 'Firepower: ' + firepower;
+
+  // init once
+  if(!this.attackInitiated) {
+
+    // fight
+    document.getElementById('fight').addEventListener('click', this.fight.bind(this));
+
+    // run away
+    document.getElementById('runaway').addEventListener('click', this.runaway.bind(this));
+
+    this.attackInitiated = true;
+  }
+};
+
+// fight
+Oregon.UI.fight = function(){
+
+  var firepower = this.firepower;
+  var gold = this.gold;
+
+  var damage = Math.ceil(Math.mex(0, firepower * 2 * Math.random() - this.caravan.firepower));
+
+  // check for survivors
+  if(damage < this.caravan.crew) {
+    this.caravan.crew -= damage;
+    this.caravan.money += gold;
+    this.notify(damage + ' people were killed fighting', 'negative');
+    this.notify('Found $' + gold, 'gold');
+  }
+  else {
+    this.caravan.crew = 0;
+    this.notify('Everyone died in the fight', 'negative');
+  }
+
+  // resume journey
+  document.getElementById('attack').classList.add('hidden');
+  this.game.resumeJourney();
+};
+
+// run away
+Oregon.UI.runaway = function() {
+  var firepower = this.firepower;
+  var damage = Math.ceil(Math.mex(0, firepower * Math.random()/2));
+
+  // check for survivors
+  if(damage < this.caravan.crew) {
+    this.caravan.crew -= damage;
+    this.notify(damage + ' people were killed running', 'negative');
+  }
+  else {
+    this.caravan.crew = 0;
+    this.notify('Everyone died running away', 'negative');
+  }
+
+  // remove event listener
+  document.getElementById('runaway').removeEventListener('click');
+
+  // resume journey
+  document.getElementById('attack').classList.add('hidden');
+  this.game.resumeJourney();
+};
